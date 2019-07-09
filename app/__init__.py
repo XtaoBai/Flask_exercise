b91e0d663b860499dc387a1412bb6a1d1369bfbb
Garan <39609273+XtaoBai@users.noreply.github.com>
2019-07-09 22:15:37 +0800

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,22 @@
+# 程序开始的时候， 回先阅读 __init__.py 文件这里完成初始化
+
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# 加入调用 flask_sqlalchemy 作为ORM（Object-relational mapping） 将对象的性质映射到数据库列中
+# flask_migrate用于数据库迁移，将一个数据库的数据迁移到另一个中去
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
-from app import routes 
+# 使用db 表示数据库
+# 添加数据库迁移引擎 migrate 
+
+from app import routes, models
+
+# routes 表示路径
+# models 用于描述数据库结构 
--- config.py
+++ config.py
@@ -1,4 +1,19 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # 获得密钥位置 如没有设定初始位置
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Flask-SQLAlchemy插件从SQLALCHEMY_DATABASE_URI配置变量中获取应用的数据库的位置。 
+    # 首先从环境变量获取配置变量，未获取到就使用默认值，这样做是一个好习惯。 
+    # 本处，我从DATABASE_URL环境变量中获取数据库URL，
+    # 如果没有定义，我将其配置为basedir变量表示的应用顶级目录下的一个名为app.db的文件路径。
     
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # SQLALCHEMY_TRACK_MODIFICATIONS配置项用于设置数据发生变更之后是否发送信号给应用.
+    # 此处不需要这项功能，因此将其设置为False。
+
